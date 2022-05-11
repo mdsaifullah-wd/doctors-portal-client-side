d1d8f21bf253aab726351dcbfe48fbff0b55b5a1
mdsaifullah.wd <mdsaifullah.wd@gmail.com>
2022-05-11 08:15:31 +0600

    update navbar

--- src/Pages/Shared/Navbar.js
+++ src/Pages/Shared/Navbar.js
@@ -2,14 +2,36 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Navbar = () => {
+  const menuItem = (
+    <>
+      <li>
+        <Link to='/'>Home</Link>
+      </li>
+      <li>
+        <Link to='/about'>About</Link>
+      </li>
+      <li>
+        <Link to='/appointment'>Appointment</Link>
+      </li>
+      <li>
+        <Link to='/reviews'>Reviews</Link>
+      </li>
+      <li>
+        <Link to='/contact-us'>Contact Us</Link>
+      </li>
+      <li>
+        <Link to='/login'>Login</Link>
+      </li>
+    </>
+  );
   return (
-    <div class='navbar bg-base-100'>
-      <div class='navbar-start'>
-        <div class='dropdown'>
-          <label tabindex='0' class='btn btn-ghost lg:hidden'>
+    <div className='navbar'>
+      <div className='navbar-start'>
+        <div className='dropdown'>
+          <label tabindex='0' className='btn btn-ghost lg:hidden'>
             <svg
               xmlns='http://www.w3.org/2000/svg'
-              class='h-5 w-5'
+              className='h-5 w-5'
               fill='none'
               viewBox='0 0 24 24'
               stroke='currentColor'>
@@ -23,53 +45,16 @@ const Navbar = () => {
           </label>
           <ul
             tabindex='0'
-            class='menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52'>
-            <li>
-              <Link to='/'>Home</Link>
-            </li>
-            <li>
-              <Link to='/about'>About</Link>
-            </li>
-            <li>
-              <Link to='/appointment'>Appointment</Link>
-            </li>
-            <li>
-              <Link to='/reviews'>Reviews</Link>
-            </li>
-            <li>
-              <Link to='/contact-us'>Contact Us</Link>
-            </li>
-            <li>
-              <Link to='/login'>Login</Link>
-            </li>
+            className='menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52'>
+            {menuItem}
           </ul>
         </div>
-        <a class='btn btn-ghost normal-case text-xl'>daisyUI</a>
+        <Link to='/' className='btn btn-ghost normal-case text-xl'>
+          Doctors Portal
+        </Link>
       </div>
-      <div class='navbar-center hidden lg:flex'>
-        <ul class='menu menu-horizontal p-0'>
-          <li>
-            <Link to='/'>Home</Link>
-          </li>
-          <li>
-            <Link to='/about'>About</Link>
-          </li>
-          <li>
-            <Link to='/appointment'>Appointment</Link>
-          </li>
-          <li>
-            <Link to='/reviews'>Reviews</Link>
-          </li>
-          <li>
-            <Link to='/contact-us'>Contact Us</Link>
-          </li>
-          <li>
-            <Link to='/login'>Login</Link>
-          </li>
-        </ul>
-      </div>
-      <div class='navbar-end'>
-        <a class='btn'>Get started</a>
+      <div className='navbar-end hidden lg:flex'>
+        <ul className='menu menu-horizontal p-0'>{menuItem}</ul>
       </div>
     </div>
   );
