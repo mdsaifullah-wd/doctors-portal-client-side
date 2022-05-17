eecdf467da96bca0d696b226447bad8ceadedefb
mdsaifullah.wd <mdsaifullah.wd@gmail.com>
2022-05-17 16:04:08 +0600

    show available slots for a day

--- src/Pages/Appointment/AvailableAppointments.js
+++ src/Pages/Appointment/AvailableAppointments.js
@@ -5,16 +5,17 @@ import AppointmentServiceCard from './AppointmentServiceCard';
 import BookingModal from './BookingModal';
 
 const AvailableAppointments = ({ selectedDay }) => {
+  const date = format(selectedDay, 'PP');
   const [services, setServices] = useState([]);
   const [treatment, setTreatment] = useState(null);
-  const local = 'http://localhost:3001/services';
+  const local = `http://localhost:3001/available/?date=${date}`;
   useEffect(() => {
     axios.get(local).then((res) => setServices(res.data));
-  }, []);
+  }, [local]);
   return (
     <section className='container my-20'>
       <h4 className='text-center text- text-secondary text-xl font-bold mb-16'>
-        Available Appointments on {format(selectedDay, 'PP')}
+        Available Appointments on {date}
       </h4>
 
       <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-9'>
